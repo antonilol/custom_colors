815dd6ef09451f07e46de1922dd2b725f97c4b20
antonilol <49868160+antonilol@users.noreply.github.com>
2019-05-30 17:08:50 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-//% color="#000000"
+// You can't edit all namespaces.
+//% color="#000000" icon="B"
 namespace basic { }
 
 //% color="#000000"
@@ -7,14 +8,11 @@ namespace input { }
 //% color="#000000"
 namespace music { }
 
-//% color="#000000"
+//% color=#7600A8 icon="\uf205"
 namespace led { }
 
-//% color="#000000"
+//% color="#E3008C" icon="\uf012"
 namespace radio { }
-
-//% color="#000000"
-namespace Math { }
 
 //% color="#000000"
 namespace game { }
@@ -28,5 +26,5 @@ namespace pins { }
 //% color="#000000"
 namespace serial { }
 
-//% color="#000000"
+//% color="#333333" icon="\uf233"
 namespace control { }
