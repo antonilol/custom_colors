559a16916f00c6fa9535f776723479a94a5b8ee2
antonilol <49868160+antonilol@users.noreply.github.com>
2019-05-30 17:01:00 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,2 +1,17 @@
-//% color="#000"
-namespace basic{}
+//% color="#000000"
+namespace basic { }
+
+//% color="#000000"
+namespace input { }
+
+//% color="#000000"
+namespace music { }
+
+//% color="#000000"
+namespace led { }
+
+//% color="#000000"
+namespace radio { }
+
+//% color="#000000"
+namespace logic { }
