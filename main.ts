42f1ef256fb574ca39b5c91808a5602fda06027b
antonilol <49868160+antonilol@users.noreply.github.com>
2019-05-30 17:15:23 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@
 //% color="#000000" icon="B"
 namespace basic { }
 
-//% color="#000000"
+//% color="#D400D4" icon="\uf192"
 namespace input { }
 
-//% color="#000000"
+//% color="#000000" icon="M"
 namespace music { }
 
 //% color=#7600A8 icon="\uf205"
@@ -14,16 +14,16 @@ namespace led { }
 //% color="#E3008C" icon="\uf012"
 namespace radio { }
 
-//% color="#000000"
+//% color="#007A4B" icon="\uf11b"
 namespace game { }
 
-//% color="#000000"
+//% color="#7600A8" icon="\uf03e"
 namespace images { }
 
-//% color="#000000"
+//% color="#B22222" icon="\uf140"
 namespace pins { }
 
-//% color="#000000"
+//% color="#002050" icon="\uf287"
 namespace serial { }
 
 //% color="#333333" icon="\uf233"
